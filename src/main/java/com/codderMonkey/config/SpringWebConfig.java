b4fe99adb230921ea0ad7f14693ff530622a1906
jajendrapandeya <=>
2020-09-07 21:03:28 +0545

    Configurations done

---
+++ src/main/java/com/codderMonkey/config/SpringWebConfig.java
@@ -0,0 +1,39 @@
+
+package com.codderMonkey.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author gajen
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.codderMonkey"})
+@EnableWebMvc
+public class SpringWebConfig extends WebMvcConfigurerAdapter{
+
+    //Access static resources over here
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //TODO
+    }
+    
+    @Bean
+    public ViewResolver viewResolver() {
+        
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setViewClass(JstlView.class);
+        vr.setPrefix("/WEB-INF/view/");
+        vr.setSuffix(".jsp");
+        return vr;
+       
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : hello
+    Created on : Sep 6, 2020, 10:20:58 PM
+    Author     : gajen
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!
+            Spring Ready Folks!<br>
+        </h1>
+    </body>
+</html>
