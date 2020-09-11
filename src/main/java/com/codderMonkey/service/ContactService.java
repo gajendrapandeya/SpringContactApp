7b1f69fedbd6a582cda8317ea93bf846b3ad7b9a
jajendrapandeya <=>
2020-09-11 20:55:25 +0545

    Implemented ContactService

---
+++ src/main/java/com/codderMonkey/service/ContactService.java
@@ -0,0 +1,38 @@
+
+package com.codderMonkey.service;
+
+import com.codderMonkey.domain.Contact;
+import java.util.List;
+
+/**
+ *
+ * @author gajen
+ */
+public interface ContactService {
+    
+    public void save(Contact c);
+    public void update(Contact c);
+    public void delete(Integer contactId);
+    
+    //used to delete bulk of contacts
+    public void delete(Integer[] contactIds);
+    
+    /**
+     * This method returns all User Contact (user who is logged in).
+     *
+     * @param userId
+     * @return
+     */
+    public List<Contact> findUserContact(Integer userId);
+    
+    /**
+     * The method search contact for user(userId) based on given
+     * free-text-criteria (txt)
+     *
+     * @param userId User who is logged in
+     * @param txt criteria used to search - free text search criteria
+     * @return
+     */
+    public List<Contact> findUserContact(Integer userId, String txt);
+    
+}
